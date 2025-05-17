206f47f2525fddaeca6e7f1bdc7c8a969bb398ea
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-05-17 20:49:47 +0700

    member

--- destructor.cpp
+++ destructor.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
 
-
-
-
-
-
-
-
-
-
+class angka {
+private:
+    int *arr;
+    int panjang;
+public:
+    angka(int); //Constructor
+    ~angka(); //Destructor
+    void cetakData();
+    void isiData();
+};
 
 // Denfinisi member Function
-angka::angka(int i){ // Constructor
-    panjang = i;
-    arr = new int[i];
-    isiData();
-}
+
+
+
+
 
  angka::~angka() { //Destructor
     cout<<endl;
