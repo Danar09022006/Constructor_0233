4532e40b39a6c911ef086439480776b03b3dcf4e
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-05-17 20:58:33 +0700

    menampilkan data

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -19,31 +19,30 @@ mahasiswa :: mahasiswa(){
     nama = "";
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int main(){
-    mahasiswa mhs1;
-    mahasiswa mhs2(20);
-    mahasiswa mhs3("Indra");
-    mahasiswa mhs4(30,"Fauzan");
-
-    mhs1.cetak();
-    mhs2.cetak();
-    mhs3.cetak();
-    mhs4.cetak();
-
-    return 0;
+mahasiswa :: mahasiswa(int iNim){
+nim =iNim;
 }
+
+mahasiswa :: mahasiswa(string iNama){
+nama=iNama;
+}
+
+mahasiswa :: mahasiswa(int iNim, string iNama){
+nim=iNim;
+nama=iNama;
+}
+void mahasiswa::cetak(){
+cout<<endl<< "nim =" <<nim<<endl;
+cout<< " Nama = " << nama<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
