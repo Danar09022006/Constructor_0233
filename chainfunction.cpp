813af246d728a6621ee522043e5bcb4553f7d8fc
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-05-17 16:54:56 +0700

    pull

---
+++ chainfunction.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class buku
+
+{
+    string Judul;
+
+
+public:
+    buku setJudul(string Judul)
+    {
+        this->Judul = Judul;
+        return *this; // chain function
+    }
+    string getJudul()
+    {
+        return this->Judul;
+    }
+} bukunya;
+
+int main()
+{
+    // bukunya.setJudul("Matematika");
+    // cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
+    return 0;
+}
