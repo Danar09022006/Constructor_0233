da97328aedd407bcf3f3a026c50e61901552922c
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-05-17 20:59:53 +0700

    member class

--- mbrstatikfunc.cpp
+++ mbrstatikfunc.cpp
@@ -2,12 +2,11 @@
 #include <string>
 using namespace std;
 
-class mahasiswa{
-private:
-    static int nim;
-public:
-    int id;
-    string nama;
+
+
+
+
+
 
     void setID();
     void printALL();
