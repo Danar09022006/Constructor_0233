dcf60fdcdf1e2e98eafd3c2d8773a58e77f70e8d
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-05-17 21:04:14 +0700

    print

--- mbrstatikfunc.cpp
+++ mbrstatikfunc.cpp
@@ -18,16 +18,17 @@ public:
     mahasiswa(string pnama):nama(pnama){setID();}
 };
 
+ int mahasiswa ::nim=0;
 
-
-
+ void mahasiswa::setID(){
+    id = ++nim;
 }
 
-void mahasiswa::printALL(){
-    cout<<"ID  = "<<id<<endl;
-    cout<<"Nama  = "<<nama<<endl;
-    cout<<endl;
-}
+
+
+
+
+
 
 int main(){
     mahasiswa mhs1("Sri Dadi");
