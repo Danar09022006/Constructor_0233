f8e30af2eef1f0484b08111ac49e73f5bed832d0
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-05-17 21:00:37 +0700

    function

--- mbrstatikfunc.cpp
+++ mbrstatikfunc.cpp
@@ -2,19 +2,19 @@
 #include <string>
 using namespace std;
 
+class mahasiswa{
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
 
-
-
-
-
-
-    void setID();
-    void printALL();
-
-    static void setNim(int pNim){nim=pNim; /*Definisi Function*/}
-    static int getNim(){return nim; /*Definisi Function*/}
     
-    mahasiswa(string pnama):nama(pnama){setID();}
+    
+
+    
+    
+    
 };
 
 int mahasiswa ::nim=0;
